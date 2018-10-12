b3b32c1937562d2f5405018b1074ca4286a37580
Simon Deziel <simon@sdeziel.info>
2018-10-12 00:02:51 -0400

    nginx::conf: add show_diff to avoid displaying/logging sensitive diff

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -14,6 +14,7 @@ define nginx::conf (
   Optional[String] $owner          = undef,
   Optional[String] $group          = undef,
   Optional[String] $mode           = '0644',
+  Boolean $show_diff               = true,
 ) {
   if $source and $content {
     fail("${module_name} source and content cannot be use at the same time")
@@ -25,16 +26,17 @@ define nginx::conf (
 
   # conf.d
   file { "/etc/nginx/conf.d/${name}":
-    ensure  => $ensure,
-    content => $content,
-    source  => $source,
-    owner   => $owner,
-    group   => $group,
-    mode    => $mode,
-    notify  => Exec["service nginx ${service_action}"],
+    ensure    => $ensure,
+    content   => $content,
+    source    => $source,
+    owner     => $owner,
+    group     => $group,
+    mode      => $mode,
+    show_diff => $show_diff,
+    notify    => Exec["service nginx ${service_action}"],
     # XXX: "before" is required to prevent a race when a site starts referencing a
     #      new conf file. If the site change is deployed first, it will trigger a
     #      reload which will trip on the missing conf that is about to be deployed
-    before  => Exec['service nginx reload'],
+    before    => Exec['service nginx reload'],
   }
 }
